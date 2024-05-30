2679b2cf700f383a869592a026fd39af14f2b278
isaac <isaacliljeros@intelliwake.com>
2024-05-30 15:25:27 -0400

    Sessions added

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,11 +1,51 @@
-import type { Handle } from '@sveltejs/kit'
+import { type Cookies, type Handle, redirect } from '@sveltejs/kit'
 import { db } from '$lib/server/db/db'
+import { Database } from 'sqlite'
+
+async function loadUserFromSession(
+	cookies: Cookies
+): Promise<{ id: number; name: string; email: string } | undefined> {
+	const sesssionID = cookies.get('sessionID')
+
+	if (sesssionID === undefined) return undefined
+
+	return { id: 0, name: '', email: '' }
+}
+
+async function sessionValid(db: Database, cookies: Cookies): Promise<boolean> {
+	const sessionID = cookies.get('sessionID')
+	if (sessionID === undefined) return false
+
+	const { expires_at } =
+		(await db.get<{ expires_at: number }>(
+			'select expires_at from session where session_uuid = ?',
+			sessionID
+		)) ?? {}
+
+	const isValid = expires_at !== undefined && expires_at >= new Date().getTime()
+	console.log(expires_at, new Date().getTime(), isValid)
+
+	if (isValid) {
+		// extend
+	} else {
+		cookies.delete('sessionID', { path: '/' })
+	}
+
+	return isValid
+}
 
 export const handle: Handle = async ({ event, resolve }) => {
-	// check for authentication and push to login if not authenticated
-
 	// Add db connection
 	event.locals.db = db
+
+	const path = event.url.pathname
+	if (await sessionValid(db, event.cookies)) {
+		if (path === '' || path === '/') redirect(307, '/home')
+	} else {
+		if (path !== '' && path !== '/') redirect(307, '/')
+	}
+
+	const user = await loadUserFromSession(event.cookies)
 
 	return resolve(event)
 }
